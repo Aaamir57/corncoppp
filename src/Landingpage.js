19bf1ed1930e6665e40ce6efa7e7dbf49fc4544b
Amir <amirkhurshid57@gmail.com>
2023-10-23 22:29:57 +0500

    landing page after correction 2

--- src/Landingpage.js
+++ src/Landingpage.js
@@ -490,6 +490,91 @@ export default function Landingpage() {
             <div class="container text-center">
                 <div class="row">
                     <div class="col">
+                        <div className='portfoliosetttt'>
+                            <div className='portfolio pooo' >
+                                <img src="./media/64.svg" />
+                                <div className='portfoliotext tttt'>
+                                    <h1>Alex Johnson</h1>
+                                    <h2>UI/UX Designer</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation
+                                        ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                </div>
+                            </div>
+                            <div className='portfolio pooo'>
+                                <img src="./media/64.svg" />
+                                <div className='portfoliotext tttt'>
+                                    <h1>Alex Johnson</h1>
+                                    <h2>UI/UX Designer</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation
+                                        ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                </div>
+                            </div>
+                            <div className='portfolio pooo'>
+                                <img src="./media/64.svg" />
+                                <div className='portfoliotext tttt'>
+                                    <h1>Alex Johnson</h1>
+                                    <h2>UI/UX Designer</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation
+                                        ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className='portfoliosetttt TY'>
+                            
+                            <div className='portfolio pooo'>
+                                <img src="./media/64.svg" />
+                                <div className='portfoliotext tttt'>
+                                    <h1>Alex Johnson</h1>
+                                    <h2>UI/UX Designer</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation
+                                        ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                </div>
+                            </div>
+
+                        
+                            <div className='portfolio pooo'>
+                                <img src="./media/64.svg" />
+                                <div className='portfoliotext tttt'>
+                                    <h1>Alex Johnson</h1>
+                                    <h2>UI/UX Designer</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation
+                                        ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                </div>
+                            </div>
+                            <div className='portfolio pooo'>
+                                <img src="./media/64.svg" />
+                                <div className='portfoliotext tttt'>
+                                    <h1>Alex Johnson</h1>
+                                    <h2>UI/UX Designer</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation
+                                        ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                       
+                          
+                        
+                    </div>
+
+                </div>
+            </div>
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
                         <button className='viewbtn'>
                             <a>View All</a>
 
